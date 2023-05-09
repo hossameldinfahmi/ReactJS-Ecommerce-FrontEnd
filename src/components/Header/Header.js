27a7c61e9f7a31f30d264d6ca77fce60c2365e8c
hossameldinfahmi <hossameldinfahmi@gmail.com>
2023-05-09 18:21:20 +0300

    Create Header Component UI

---
+++ src/components/Header/Header.js
@@ -0,0 +1,13 @@
+import Carousel from "../Carousel/Carousel";
+import NavBar from "../NavbarMenu/NavbarMenu";
+
+export default function Example() {
+  return (
+    <>
+      <header>
+        <NavBar />
+        <Carousel />
+      </header>
+    </>
+  );
+}
