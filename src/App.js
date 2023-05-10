ac80bf7ee540cb0a8b652dbdc169d675556f6b51
hossameldinfahmi <hossameldinfahmi@gmail.com>
2023-05-10 21:02:28 +0300

    Resolve Conflect

--- src/App.js
+++ src/App.js
@@ -5,18 +5,17 @@ import Home from "./pages/Home/Home";
 import Footer from "./components/Footer/Footer";
 import NavBar from "./components/NavbarMenu/NavbarMenu";
 import Products from "./pages/Products/Products";
-
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import RegistrationForm from "./pages/Registration/RegistrationForm";
 
 function App() {
   return (
     <>
+      <ToastContainer />
+
       <Router>
         <NavBar />
         <Routes>
-          <ToastContainer />
-
           <Route path="/" element={<Home />} />
           <Route path="/products" element={<Products />} />
           <Route path="/product/:id"> </Route>
