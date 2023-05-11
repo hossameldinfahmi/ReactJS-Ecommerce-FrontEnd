cc97f4c657594d8b697dc4a409bf51c6bc82b5c0
Islam Sulaiman <ma.eslam@yahoo.com>
2023-05-11 22:44:56 +0300

    single product page

--- src/pages/singleProduct/SingleProduct.js
+++ src/pages/singleProduct/SingleProduct.js
@@ -1,19 +1,25 @@
 import React from "react";
 import { useParams } from "react-router-dom";
 import { useSelector } from "react-redux";
+// import do
 
 
 const SingleProduct = () =>{
 
     const {id} = useParams();
+    console.log(`id ${id}`)
     
     const products = useSelector((state)=> state.products.items)
+    console.log(`prod ${products}`)
     const oneProduct = products.find((product)=> product.id === parseInt(id))
+    console.log(oneProduct)
+    const url = process.env.REACT_APP_IMAGE_BASE_URL
+    console.log(typeof url)
 
 return (
     <>
         <div> 
-            <img src={oneProduct.image} alt="product"></img>
+            <img src={`${url}${oneProduct.image}`} alt="product"></img>
         </div>
         <h3>{oneProduct.name}</h3>
         <h3>{oneProduct.description}</h3>
