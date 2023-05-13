9e5588854418af651876d505da1edb7c1c731466
hossameldinfahmi <hossameldinfahmi@gmail.com>
2023-05-13 19:07:28 +0300

    Create Change address, email, password Functions

---
+++ src/components/ChangePassword/ChangePassword.js
@@ -0,0 +1,132 @@
+import React, { useEffect } from "react";
+import { Formik, Form, Field, ErrorMessage } from "formik";
+import axios from "axios";
+import * as Yup from "yup";
+import { toast } from "react-toastify";
+import { useNavigate } from "react-router-dom";
+import { logout } from "../../store/authSlice/login";
+import { useDispatch } from "react-redux";
+function ChangePasswordForm() {
+  const dispatch = useDispatch();
+
+  const initialValues = {
+    email: "",
+    password: "",
+    confirmPassword: "",
+  };
+  const token = localStorage.getItem("token");
+  const navigate = useNavigate();
+
+  const validationSchema = Yup.object().shape({
+    email: Yup.string().email("Invalid email address").required("Required"),
+    password: Yup.string().required("Required"),
+    confirmPassword: Yup.string()
+      .oneOf([Yup.ref("password"), null], "Passwords must match")
+      .required("Required"),
+  });
+  const handleLogout = () => {
+    localStorage.removeItem("token");
+    dispatch(logout());
+  };
+
+  const onSubmit = async (values, { setSubmitting }) => {
+    setSubmitting(true);
+    try {
+      const response = await axios.patch(
+        `${process.env.REACT_APP_BASE_API_URL}/auth/`,
+        {
+          email: values.email,
+          password: values.password,
+          confirm_password: values.confirmPassword,
+        },
+        {
+          headers: {
+            Authorization: `Bearer ${token}`,
+          },
+        }
+      );
+      toast.success("Password Changed Succsesuflly", {
+        position: toast.POSITION.TOP_RIGHT,
+      });
+      handleLogout();
+
+      navigate("/login");
+    } catch (error) {
+      toast.error(error, {
+        position: toast.POSITION.TOP_RIGHT,
+      });
+    }
+    setSubmitting(false);
+  };
+
+  useEffect(() => {
+    window.scrollTo(0, 0);
+  }, []);
+
+  return (
+    <Formik
+      initialValues={initialValues}
+      validationSchema={validationSchema}
+      onSubmit={onSubmit}
+      className="container"
+    >
+      {(formik) => (
+        <Form>
+          <h2 className="text-4xl font-extrabold my-6">Change Password</h2>
+
+          <div className="mb-4">
+            <label htmlFor="email">Email</label>
+            <Field
+              type="email"
+              id="email"
+              name="email"
+              className="block w-full p-2 border rounded-md"
+            />
+            <ErrorMessage
+              name="email"
+              component="div"
+              className="text-red-500"
+            />
+          </div>
+          <div className="mb-4">
+            <label htmlFor="password">New Password</label>
+            <Field
+              type="password"
+              id="password"
+              name="password"
+              className="block w-full p-2 border rounded-md"
+            />
+            <ErrorMessage
+              name="password"
+              component="div"
+              className="text-red-500"
+            />
+          </div>
+          <div className="mb-4">
+            <label htmlFor="confirmPassword">Confirm Password</label>
+            <Field
+              type="password"
+              id="confirmPassword"
+              name="confirmPassword"
+              className="block w-full p-2 border rounded-md"
+            />
+            <ErrorMessage
+              name="confirmPassword"
+              component="div"
+              className="text-red-500"
+            />
+          </div>
+          <button
+            type="submit"
+            className="bg-green-500 text-white px-4 py-2 rounded-full"
+            disabled={!formik.isValid || formik.isSubmitting}
+          >
+            Submit
+          </button>
+        </Form>
+      )}
+    </Formik>
+  );
+}
+
+export default ChangePasswordForm;
--- src/pages/UserProfile/UserProfile.js
+++ src/pages/UserProfile/UserProfile.js
@@ -2,14 +2,22 @@ import React from "react";
 import { useSelector } from "react-redux";
 import { useNavigate } from "react-router-dom";
 import { useEffect } from "react";
+import {
+  Tabs,
+  TabsHeader,
+  TabsBody,
+  Tab,
+  TabPanel,
+} from "@material-tailwind/react";
+import ChangePasswordForm from "../../components/ChangePassword/ChangePassword";
+import ChangeEmailForm from "../../components/ChangeEmail/ChangeEmail";
+import ChangeAddressForm from "../../components/ChangeAddress/ChangeAddress";
 
 function UserProfile() {
   const isLoggedIn = useSelector((state) => state.auth.isLoggedIn);
   const navigate = useNavigate();
   const { user } = useSelector((state) => state.auth);
-
   console.log(user);
-
   useEffect(() => {
     if (!isLoggedIn) {
       navigate("/login");
@@ -17,9 +25,9 @@ function UserProfile() {
   }, [isLoggedIn, navigate]);
   return (
     <div className="p-16">
-      <div className="p-8 bg-my-blue shadow mt-24 flex flex-col md:flex-row items-center">
+      <div className="p-8 bg-gray-100 rounded-full shadow-md mt-24 flex flex-col md:flex-row items-center">
         <div className="relative">
-          <div className="w-48 overflow-hidden h-48 bg-indigo-100 mx-16 mb-14 rounded-full shadow-2xl flex items-center justify-between text-indigo-500 ">
+          <div className="w-48 overflow-hidden h-48 bg-indigo-100 mx-16  rounded-full shadow-2xl flex items-center justify-between text-indigo-500 ">
             <img
               src={
                 user && user.image
@@ -31,12 +39,12 @@ function UserProfile() {
             />
           </div>
         </div>
-        <div className="md:ml-8 mt-8 md:mt-0 text-center md:text-left border-b pb-12">
+        <div className="md:ml-8 mt-8 md:mt-0 text-center md:text-left border-b ">
           <h1 className="text-4xl font-medium text-gray-700">
             Name : {user?.username}
           </h1>
           <p className="font-light text-gray-600 mt-3">
-            DOB : {user?.date_of_birth}
+            Date Of Birth : {user?.date_of_birth}
           </p>
           <p className="mt-8 text-gray-500">Email: {user?.email}</p>
           <p className="mt-2 text-gray-500">Phone: {user?.phone}</p>
@@ -44,15 +52,48 @@ function UserProfile() {
         </div>
       </div>
       <div className="mt-12 flex flex-col justify-center">
-        <p className="text-gray-600 text-center md:text-left font-light lg:px-16">
-          An artist of considerable range, Ryan — the name taken by
-          Melbourne-raised, Brooklyn-based Nick Murphy — writes, performs and
-          records all of his own music, giving it a warm, intimate feel with a
-          solid groove structure. An artist of considerable range.
-        </p>
-        <button className="text-indigo-500 py-2 px-4 font-medium mt-4">
-          Show more
-        </button>
+        <Tabs id="custom-animation" value="html">
+          <TabsHeader>
+            <Tab
+              className="bg-gray-900 text-white py-2  mx-2 px-4 rounded-l-md rounded-full"
+              selectedClassName="bg-blue-600"
+              value="Password"
+            >
+              Change Password
+            </Tab>
+            <Tab
+              className="bg-gray-900 text-white  mx-2 py-2 px-4 rounded-full"
+              selectedClassName="bg-blue-600"
+              value="Email"
+            >
+              Change Email
+            </Tab>
+            <Tab
+              className="bg-gray-900 text-white mx-2 py-2 px-4 rounded-r-md rounded-full"
+              selectedClassName="bg-blue-600"
+              value="Address"
+            >
+              Change Address
+            </Tab>
+          </TabsHeader>
+          <TabsBody
+            animate={{
+              initial: { y: 250 },
+              mount: { y: 0 },
+              unmount: { y: 250 },
+            }}
+          >
+            <TabPanel value="Password">
+              <ChangePasswordForm />
+            </TabPanel>
+            <TabPanel value="Email">
+              <ChangeEmailForm />
+            </TabPanel>
+            <TabPanel value="Address">
+              <ChangeAddressForm />
+            </TabPanel>
+          </TabsBody>
+        </Tabs>
       </div>
     </div>
   );
