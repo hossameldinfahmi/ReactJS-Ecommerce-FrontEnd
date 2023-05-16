97b0bd9c38220f6d144b9f8af50f999634dba81b
Islam Sulaiman <ma.eslam@yahoo.com>
2023-05-16 23:14:15 +0300

    Functionality: add button for adding products to cart

--- src/components/Product/Product.js
+++ src/components/Product/Product.js
@@ -1,11 +1,19 @@
 import React from "react";
 import { Link } from "react-router-dom";
+import { addItemToCart } from "../../store/cart/cart-actions";
+import { useDispatch } from "react-redux";
  
 
 const Product = ({id, category, name, description, price, available_quatity, image}) =>{
 
-  const imageUrl = process.env.REACT_APP_IMGE_API_URL
+  const dispatch = useDispatch();
 
+  const imageUrl = process.env.REACT_APP_IMGE_API_URL
+  const product = {id}
+
+  const addToCart = () => {
+    dispatch(addItemToCart(product))
+  }
 
   return(
     <>
@@ -16,6 +24,8 @@ const Product = ({id, category, name, description, price, available_quatity, ima
         <h3>{name}</h3>
         <h3>{price}</h3>
         <h3>{description}</h3>
+        <button onClick={addToCart}>Add to Cart</button>
+        <br />
         <Link to={`/product/${id}`} className="btn">Details </Link>
       </div>
 
