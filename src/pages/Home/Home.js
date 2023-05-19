34e732807b48e31a62f6db12f0e9f6f91e5be574
Moustafa25MM <107191669+Moustafa25MM@users.noreply.github.com>
2023-05-19 07:31:04 +0300

    add Product Carousel

--- src/pages/Home/Home.js
+++ src/pages/Home/Home.js
@@ -6,6 +6,8 @@ import PartnarsCards from "../../components/Static/PartnarsCards/PartnarsCards";
 import BannarSection from "../../components/Static/BannarSection/BannarSection";
 import SlideCards from "../../components/Static/SlideCards/SlideCards";
 import ProductCard from "../../components/Static/ProductCard/ProductCard";
+import ProductCarousel from "../../components/ProductCarousel/ProductCarousel";
+
 import { fetchUserData } from "../../store/authSlice/login";
 import { useEffect } from "react";
 import { fetchCartItems } from "../../store/cart/cart-actions";
